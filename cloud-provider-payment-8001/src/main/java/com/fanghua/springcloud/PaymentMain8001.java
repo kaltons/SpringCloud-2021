fd8c2c21ead2f56d1bdc6afdc4d77536e77f7245
fcwei <chuangwei.fang@3dxt.com>
2021-09-14 14:27:01 +0800

    feat: 初始化子工程
    1、修改POM文件
    2、编写YML文件
    3、添加主启动类

---
+++ cloud-provider-payment-8001/src/main/java/com/fanghua/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 支付微服务主程序
+ *
+ * @author fcwei
+ * @date 2021/9/14 - 14:17
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
