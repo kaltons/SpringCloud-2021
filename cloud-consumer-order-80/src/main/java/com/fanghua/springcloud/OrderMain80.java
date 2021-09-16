d6911940160809509270658164e8dbea49f4975c
kaltons <1907843600@qq.com>
2021-09-16 23:06:59 +0800

    feat: 消费者80 — 子工程搭建

---
+++ cloud-consumer-order-80/src/main/java/com/fanghua/springcloud/OrderMain80.java
@@ -0,0 +1,17 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 消费者主启动类
+ *
+ * @author fcwei
+ * @date 2021/9/16 - 23:03
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
