d7d51b351ea29fd9330daa99486bb18315d4acb6
fcwei <chuangwei.fang@3dxt.com>
2021-09-17 17:26:28 +0800

    feat: RestTemplate服务调用

---
+++ cloud-consumer-order-80/src/main/java/com/fanghua/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,22 @@
+package com.fanghua.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 配置类注入容器
+ *
+ * @author fcwei
+ * @date 2021/9/17 - 15:03
+ */
+@Configuration
+public class ApplicationContextConfig {
+
+    //往容器中添加一个RestTemplate
+    //RestTemplate提供了多种便捷访问远程http访问的方法
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
