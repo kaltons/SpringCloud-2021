854ba7a3b8d4664383a2f004d22107816530da28
kaltons <1907843600@qq.com>
2021-09-18 23:48:51 +0800

    feat: Eureka单机版构建

--- cloud-consumer-order-80/src/main/java/com/fanghua/springcloud/OrderMain80.java
+++ cloud-consumer-order-80/src/main/java/com/fanghua/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.fanghua.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 消费者主启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author fcwei
  * @date 2021/9/16 - 23:03
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
