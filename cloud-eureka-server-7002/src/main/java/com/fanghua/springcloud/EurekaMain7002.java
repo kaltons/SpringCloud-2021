c145710b5af5ca165092bb742efbef067d7ddb37
kaltons <1907843600@qq.com>
2021-09-19 13:50:40 +0800

    feat: Eureka集群版构建

---
+++ cloud-eureka-server-7002/src/main/java/com/fanghua/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka注册中心主程序
+ *
+ * @author fcwei
+ * @date 2021/9/18 - 22:17
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
