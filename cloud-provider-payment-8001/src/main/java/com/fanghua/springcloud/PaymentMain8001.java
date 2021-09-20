d0012cdcf58b1877937cbeb810a95e45f6acfff3
kaltons <1907843600@qq.com>
2021-09-20 10:45:25 +0800

    feat: 服务发现@EnableDiscoveryClient

--- cloud-provider-payment-8001/src/main/java/com/fanghua/springcloud/PaymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/com/fanghua/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.fanghua.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author fcwei
  * @date 2021/9/14 - 14:17
  */
+@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 public class PaymentMain8001 {
