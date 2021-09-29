9a83aa1caccc38e34b7c5d4d8483187a9fdb0231
kaltons <1907843600@qq.com>
2021-09-29 22:08:40 +0800

    feat: OpenFeign调用支付服务

---
+++ cloud-consumer-feign-order-80/src/main/java/com/fanghua/springcloud/OrderFeignMain80.java
@@ -0,0 +1,19 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 消费者主启动类
+ *
+ * @author fcwei
+ * @date 2021/9/29 - 21:43
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class OrderFeignMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFeignMain80.class, args);
+    }
+}
---
+++ cloud-consumer-feign-order-80/src/main/java/com/fanghua/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,31 @@
+package com.fanghua.springcloud.controller;
+
+import com.fanghua.springcloud.entities.CommonResult;
+import com.fanghua.springcloud.entities.Payment;
+import com.fanghua.springcloud.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 订单消费业务控制器
+ *
+ * @author fcwei
+ * @date 2021/9/29 - 21:56
+ */
+@Slf4j
+@RestController
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
+    {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
