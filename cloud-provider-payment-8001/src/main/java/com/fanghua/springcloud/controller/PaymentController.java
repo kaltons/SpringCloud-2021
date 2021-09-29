38e685055a2ec72903b542e3cc5613ae75c9a779
kaltons <1907843600@qq.com>
2021-09-29 23:09:13 +0800

    feat: OpenFeign超时控制

--- cloud-provider-payment-8001/src/main/java/com/fanghua/springcloud/controller/PaymentController.java
+++ cloud-provider-payment-8001/src/main/java/com/fanghua/springcloud/controller/PaymentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 支付控制器
@@ -80,6 +81,18 @@ public class PaymentController {
         }
 
         return this.discoveryClient;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout()
+    {
+        // 业务逻辑处理正确，但是需要耗费3秒钟
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return serverPort;
     }
 
 }
