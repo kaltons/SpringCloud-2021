9c9df33f43730e47ee10e9d3910a8436fd61ad5c
fcwei <chuangwei.fang@3dxt.com>
2021-09-29 17:25:38 +0800

    feat: LoadBalancer替换默认负载均衡算法
    1、本项目使用的SpringCloud版本为2020.0.3
    2、SpringCloud中Ribbon被替换为LoadBalancer

---
+++ cloud-consumer-order-80/src/main/java/com/fanghua/springcloud/config/CustomLoadBalancerConfiguration.java
@@ -0,0 +1,28 @@
+package com.fanghua.springcloud.config;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+/**
+ * LoadBalancer负载均衡算法切换
+ * 这里不需要@configuration注解
+ * @author fcwei
+ * @date 2021/9/29 - 16:38
+ */
+public class CustomLoadBalancerConfiguration {
+
+    @Bean
+    ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment,
+                                                            LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+
+        return new RandomLoadBalancer(loadBalancerClientFactory
+                .getLazyProvider(name, ServiceInstanceListSupplier.class),
+                name);
+    }
+}
