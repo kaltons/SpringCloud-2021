2c1246d0df98e54f32ffaa475d34de6343d55701
fcwei <chuangwei.fang@3dxt.com>
2021-09-30 18:16:40 +0800

    feat: Hystrix-80消费者服务初始化

---
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/fanghua/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,23 @@
+package com.fanghua.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Hystrix-Feign业务接口
+ *
+ * @author fcwei
+ * @date 2021/9/30 - 18:08
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface PaymentHystrixService {
+
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfoOk(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfoTimeOut(@PathVariable("id") Integer id);
+}
