bfb980808eeed3f9125f207ee02c66372d98ee68
kaltons <1907843600@qq.com>
2021-09-30 23:38:45 +0800

    feat: Hystrix支付测服务降级

--- cloud-provider-hystrix-payment-8001/src/main/java/com/fanghua/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment-8001/src/main/java/com/fanghua/springcloud/PaymentHystrixMain8001.java
@@ -2,16 +2,18 @@ package com.fanghua.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hystrix支付主启动程序
- *
+ * EnableCircuitBreaker 激活Hystrix注解
  * @author fcwei
  * @date 2021/9/30 - 15:19
  */
 @EnableEurekaClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
