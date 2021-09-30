02b6a88805e367fa782cbca574a20b6fbf3c3412
fcwei <chuangwei.fang@3dxt.com>
2021-09-30 18:16:40 +0800

    feat: Hystrix-80消费者服务初始化

---
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/fanghua/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,20 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Hystrix消费者主程序
+ *
+ * @author fcwei
+ * @date 2021/9/30 - 18:06
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class OrderHystrixMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
