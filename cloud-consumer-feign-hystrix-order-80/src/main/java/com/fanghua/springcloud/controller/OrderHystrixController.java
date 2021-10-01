6c717777ee3cc10e6fb370e342762753bcb5e0f4
kaltons <1907843600@qq.com>
2021-10-01 22:23:16 +0800

    feat: Hystrix全局服务降级

--- cloud-consumer-feign-hystrix-order-80/src/main/java/com/fanghua/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order-80/src/main/java/com/fanghua/springcloud/controller/OrderHystrixController.java
@@ -1,8 +1,8 @@
 package com.fanghua.springcloud.controller;
 
 import com.fanghua.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +18,7 @@ import javax.annotation.Resource;
  */
 @Slf4j
 @RestController
+@DefaultProperties(defaultFallback = "paymentGlobalFallbackMethod")
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -28,10 +29,8 @@ public class OrderHystrixController {
         return paymentHystrixService.paymentInfoOk(id);
     }
 
+    @HystrixCommand
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
-    })
     public String paymentInfoTimeOut(@PathVariable("id") Integer id) {
         // int age = 10/0;
         return paymentHystrixService.paymentInfoTimeOut(id);
@@ -42,5 +41,13 @@ public class OrderHystrixController {
      */
     public String paymentTimeOutFallbackMethod(@PathVariable("id") Integer id){
         return "我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
+    }
+
+    /**
+     * 全局fallback方法
+     */
+    public String paymentGlobalFallbackMethod()
+    {
+        return "Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~";
     }
 }
