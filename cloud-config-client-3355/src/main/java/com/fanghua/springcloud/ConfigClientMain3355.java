2732ce0abaa650e942395e771f45de1fe58aaa0c
kaltons <1907843600@qq.com>
2021-10-05 16:52:11 +0800

    feat: Config客户端搭建

---
+++ cloud-config-client-3355/src/main/java/com/fanghua/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Config-客户端配置主程序
+ *
+ * @author fcwei
+ * @date 2021/10/5 - 16:26
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
