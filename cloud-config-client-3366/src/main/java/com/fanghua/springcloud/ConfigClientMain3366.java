3a2d9596f530ab8b4375db945b3a8d069c1b484d
kaltons <1907843600@qq.com>
2021-10-06 00:38:01 +0800

    feat: 添加Bus实现动态刷新，定点通知

---
+++ cloud-config-client-3366/src/main/java/com/fanghua/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Config-客户端配置主程序
+ *
+ * @author fcwei
+ * @date 2021/10/5 - 23:03
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
