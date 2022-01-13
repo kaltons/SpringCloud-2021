6295bd4fa644c58f5f5258fcbdec8d37143c19fc
kaltons <1907843600@qq.com>
2022-01-13 17:50:19 +0800

    feat: 构建消息驱动消费

---
+++ cloud-stream-rabbitmq-consumer-8802/src/main/java/com/fanghua/springcloud/StreamMQMain8802.java
@@ -0,0 +1,17 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务消费者主程序入口
+ *
+ * @author fcwei
+ * @date 2021/11/20 - 17:12
+ */
+@SpringBootApplication
+public class StreamMQMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class,args);
+    }
+}
