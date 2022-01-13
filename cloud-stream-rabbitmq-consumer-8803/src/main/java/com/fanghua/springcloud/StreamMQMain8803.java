f99c97196c3a98321d89203b33c3b3ab457ff04f
kaltons <1907843600@qq.com>
2022-01-13 18:19:48 +0800

    feat: 构建消息重复消费与分组

---
+++ cloud-stream-rabbitmq-consumer-8803/src/main/java/com/fanghua/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.fanghua.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务消费者主程序入口
+ *
+ * @author fcwei
+ * @date 2021/11/20 - 17:25
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
